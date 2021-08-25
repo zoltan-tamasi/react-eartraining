9bac76137e3c7230ea4b5b0521627df7a1fe50f1
Zoltan.Tamasi <mail@zoltantamasi.net>
2021-08-25 12:55:32 +0200

    Cleaned up UI code.

--- src/ui/trichordGenerator.tsx
+++ src/ui/trichordGenerator.tsx
@@ -1,51 +1,49 @@
-import React, { Dispatch, SetStateAction, useState } from 'react';
-import { Chord, Major2PlusMinor2, Major7With3, Note, NotOctaveExploded, OctaveExploded, Rotation0, Rotation1, C, D, NoteName } from '../core/TriadCore'
+import { Dispatch, SetStateAction, useState } from 'react';
 import { Action, TrichordGeneratorState } from '../state/TrichordGenerator'
 
 const useHandler = (generator: TrichordGeneratorState, setGenerator: Dispatch<SetStateAction<TrichordGeneratorState>>) =>
-    (action: Action) => setGenerator(TrichordGeneratorState.handleAction(generator, action))
+  (action: Action) => setGenerator(TrichordGeneratorState.handleAction(generator, action))
 
 const TrichordGeneratorComponent = () => {
-    const [generator, setGenerator] = useState(
-        TrichordGeneratorState.getInitial()
-    );
+  const [generator, setGenerator] = useState(
+    TrichordGeneratorState.getInitial()
+  );
 
-    const handler = useHandler(generator, setGenerator)
-     
+  const handler = useHandler(generator, setGenerator)
+
   return (
+    <div>
       <div>
-          <div>
-              Chord is: { TrichordGeneratorState.getChord(generator).notesOf().map((note) => `${note.noteName}${note.octave}`).join('\n') }
-          </div>
-          <div>
-              Intervals is: { TrichordGeneratorState.getChord(generator).intervals().map((note) => `${note}`).join(',') }
-          </div>
-          <div>
-              Basenote is: { generator.baseNote.toString() }
-          </div>
-          <div>
-              Triadcore is: { generator.triadCore.label }
-          </div>
-          <div>
-              TriadCoreLock is: { generator.triadCoreLock.locked ? 'locked' : 'open' }
-          </div>
-          <div>
-              RotationLock is: { generator.rotationLock.locked ? 'locked' : 'open' }
-          </div>
-          <div>
-              BaseNoteLock is: { generator.baseNoteLock.locked ? 'locked' : 'open' }
-          </div>
-          <div>
-              OctaveExplodeLock is: { generator.octaveExplodeLock.locked ? 'locked' : 'open' }
-          </div>
-
-          <button onClick = { () => handler({ kind: 'Randomize' }) } >Rand</button>
-          <button onClick = { () => handler({ kind: 'SwitchLock', lockKind: 'TriadCoreLock' }) } >TriadCoreLock</button>
-          <button onClick = { () => handler({ kind: 'SwitchLock', lockKind: 'RotationLock' }) } >RotationLock</button>
-          <button onClick = { () => handler({ kind: 'SwitchLock', lockKind: 'BaseNoteLock' }) } >BaseNoteLock</button>
-          <button onClick = { () => handler({ kind: 'SwitchLock', lockKind: 'OctaveExplodeLock' }) } >OctaveExplodeLock</button>
-
+        Chord is: { TrichordGeneratorState.getChord(generator).notesOf().map((note) => `${note.noteName}${note.octave}`).join('\n') }
       </div>
+      <div>
+        Intervals is: { TrichordGeneratorState.getChord(generator).intervals().map((note) => `${note}`).join(',') }
+      </div>
+      <div>
+        Basenote is: { generator.baseNote.toString() }
+      </div>
+      <div>
+        Triadcore is: { generator.triadCore.label }
+      </div>
+      <div>
+        TriadCoreLock is: { generator.triadCoreLock.locked ? 'locked' : 'open' }
+      </div>
+      <div>
+        RotationLock is: { generator.rotationLock.locked ? 'locked' : 'open' }
+      </div>
+      <div>
+        BaseNoteLock is: { generator.baseNoteLock.locked ? 'locked' : 'open' }
+      </div>
+      <div>
+        OctaveExplodeLock is: { generator.octaveExplodeLock.locked ? 'locked' : 'open' }
+      </div>
+
+      <button onClick = { () => handler({ kind: 'Randomize' }) } >Rand</button>
+      <button onClick = { () => handler({ kind: 'SwitchLock', lockKind: 'TriadCoreLock' }) } >TriadCoreLock</button>
+      <button onClick = { () => handler({ kind: 'SwitchLock', lockKind: 'RotationLock' }) } >RotationLock</button>
+      <button onClick = { () => handler({ kind: 'SwitchLock', lockKind: 'BaseNoteLock' }) } >BaseNoteLock</button>
+      <button onClick = { () => handler({ kind: 'SwitchLock', lockKind: 'OctaveExplodeLock' }) } >OctaveExplodeLock</button>
+    </div>
   );
 }
 
