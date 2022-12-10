bdd9d3b7fb744c8d57b9405fd2a870974eac800d
Zoltan Tamasi <ztamasi@chemaxon.com>
2022-12-10 20:04:25 +0100

    Added padlock icon.

--- src/ui/lock.tsx
+++ src/ui/lock.tsx
@@ -3,7 +3,7 @@ import { Action, Lock } from '../state/TrichordGenerator'
 export const LockComponent = function<T>(props: { lock: Lock<T>, handler: (action: Action) => void }) {
   return (
     <span onClick={ () => props.handler({ kind: 'SwitchLock', lockKind: props.lock.lockKind }) }>
-      { props.lock.locked ? <img src="img/locked.png"></img> : <img src="img/unlocked.png"></img> }
+      { props.lock.locked ? <img width="30" height="30" src="img/locked.png"></img> : <img width="30" height="30" src="img/unlocked.png"></img> }
     </span>
   )
 }
